e5d2a29e66637edbd70d6a1f0032d5a626eead04
KimJinHyuk <dog3027@naver.com>
2023-07-10 16:07:03 +0900

    initial setting

---
+++ src/main/java/team/qj/vocabob/VocabobApplication.java
@@ -0,0 +1,13 @@
+package team.qj.vocabob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/qj/vocabob/VocabobApplicationTests.java
@@ -0,0 +1,13 @@
+package team.qj.vocabob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
